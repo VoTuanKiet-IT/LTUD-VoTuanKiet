88e4102dc4f9fa7748a888d69a7629221b80037b
Võ Tuấn Kiệt <89638934+VoTuanKiet-IT@users.noreply.github.com>
2026-03-05 09:21:44 +0700

    first commit

---
+++ src/main/java/nhom14/VoTuanKiet/VoTuanKietApplication.java
@@ -0,0 +1,13 @@
+package nhom14.VoTuanKiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoTuanKietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoTuanKietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhom14/VoTuanKiet/VoTuanKietApplicationTests.java
@@ -0,0 +1,13 @@
+package nhom14.VoTuanKiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoTuanKietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
